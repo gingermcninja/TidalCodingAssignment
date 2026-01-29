a34c13e6fdfe14a2327c93bd804be7b923fcef4c
Paul McGrath <gingermcninja@gmail.com>
2026-01-29 16:18:52 -0500

    TID-2 Created initial project with Swift and UIKit

---
+++ TidalCodingAssignmentTests/TidalCodingAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  TidalCodingAssignmentTests.swift
+//  TidalCodingAssignmentTests
+//
+//  Created by Paul McGrath on 1/29/26.
+//
+
+import Testing
+@testable import TidalCodingAssignment
+
+struct TidalCodingAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
